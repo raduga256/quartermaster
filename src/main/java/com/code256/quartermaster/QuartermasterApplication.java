29740e7f76e05c0d06c28c5b52783a13e142ebad
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-08-18 16:58:13 +0300

    Initial commit

---
+++ src/main/java/com/code256/quartermaster/QuartermasterApplication.java
@@ -0,0 +1,13 @@
+package com.code256.quartermaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartermasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartermasterApplication.class, args);
+    }
+
+}
